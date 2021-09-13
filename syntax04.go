d476de9de60a6012e6ccfe0430960357816cecbe
hansangho0220 <wpfh220@naver.com>
2021-09-13 23:56:24 +0900

    v0.2

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-//난수 추출된 수의 소수 판정 프로그램 v0.1
+//난수 추출된 수의 소수 판정 프로그램 v0.2
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -15,13 +15,13 @@ func main() {
 	fmt.Println("임의로 추출된 수: ", number)
 
 	count := 0
-	for i := 1; i <= number; i++ {
+	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			count++
 		}
 	}
 
-	if count == 2 {
+	if count == 0 {
 		fmt.Println(number, "는 소수입니다.")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다.")
